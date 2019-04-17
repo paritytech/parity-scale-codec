0dcb9f70480082805af0c826a87a0daa963d857c
Bastian Köcher <bkchr@users.noreply.github.com>
2019-04-17 12:20:03 +0200

    Update src/codec.rs
    Co-Authored-By: cheme <emericchevalier.pro@gmail.com>

--- src/codec.rs
+++ src/codec.rs
@@ -1677,7 +1677,8 @@ mod tests {
 
 	#[test]
 	fn should_avoid_overlapping_definition() {
-		check_bound!(0b01, u8, u16, [ (u8, U8_OUT_OF_RANGE), (u16, U16_OUT_OF_RANGE),
+		check_bound!(
+			0b01, u8, u16, [ (u8, U8_OUT_OF_RANGE), (u16, U16_OUT_OF_RANGE),
 			(u32, U32_OUT_OF_RANGE), (u64, U64_OUT_OF_RANGE), (u128, U128_OUT_OF_RANGE)]
 		);
 		check_bound!(0b10, u16, u32, [ (u16, U16_OUT_OF_RANGE),
