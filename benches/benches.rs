7c04d230971adbae87c991d57c0036ab1c880a5e
thiolliere <gui.thiolliere@gmail.com>
2019-06-03 13:48:00 +0200

    Impl for u8 by specializing at compile-time (#83)
    Encode/Decode has now a new associated const that tells if the type is u8, that allow some kind of specialization when implementing Encode/Decode for `Vec<u8>`

--- benches/benches.rs
+++ benches/benches.rs
@@ -12,71 +12,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(test)]
+#[macro_use]
+extern crate criterion;
+#[macro_use]
+extern crate parity_codec_derive;
 
-extern crate test;
-
+use parity_codec::{Encode, Decode};
 use parity_codec::*;
-use parity_codec_derive::{Encode, Decode};
 
-#[bench]
-fn array_vec_write_u128(b: &mut test::Bencher) {
+use criterion::{Criterion, black_box, Bencher};
+use std::time::Duration;
+
+
+fn array_vec_write_u128(b: &mut Bencher) {
 	b.iter(|| {
-		for b in 0..test::black_box(1_000_000) {
+		for b in 0..black_box(1_000_000) {
 			let a = 0xffff_ffff_ffff_ffff_ffff_u128;
 			Compact(a ^ b).using_encoded(|x| {
-				test::black_box(x).len()
+				black_box(x).len()
 			});
 		}
 	});
 }
 
-fn test_vec<F: Fn(&mut Vec<u8>, &[u8])>(b: &mut test::Bencher, f: F) {
-	let f = test::black_box(f);
-	let x = test::black_box([0xff; 10240]);
+fn test_vec<F: Fn(&mut Vec<u8>, &[u8])>(b: &mut Bencher, f: F) {
+	let f = black_box(f);
+	let x = black_box([0xff; 10240]);
 
 	b.iter(|| {
-		for _b in 0..test::black_box(10_000) {
+		for _b in 0..black_box(10_000) {
 			let mut vec = Vec::<u8>::new();
 			f(&mut vec, &x);
 		}
 	});
 }
 
-#[bench]
-fn vec_write_as_output(b: &mut test::Bencher) {
+fn vec_write_as_output(b: &mut Bencher) {
 	test_vec(b, |vec, a| {
 		Output::write(vec, a);
 	});
 }
 
-#[bench]
-fn vec_extend(b: &mut test::Bencher) {
+fn vec_extend(b: &mut Bencher) {
 	test_vec(b, |vec, a| {
 		vec.extend(a);
 	});
 }
 
-#[bench]
-fn vec_extend_from_slice(b: &mut test::Bencher) {
+fn vec_extend_from_slice(b: &mut Bencher) {
 	test_vec(b, |vec, a| {
 		vec.extend_from_slice(a);
 	});
-}
-
-#[bench]
-fn encoding_of_large_vec_u8(b: &mut test::Bencher) {
-	let mut v = vec![];
-	for i in 0..256 {
-		v.push(i);
-	}
-	for _ in 0..12 {
-		v.extend(v.clone());
-	}
-
-	b.iter(|| {
-		v.encode();
-	})
 }
 
 #[derive(Encode, Decode)]
@@ -84,8 +70,7 @@ enum Event {
 	ComplexEvent(Vec<u8>, u32, i32, u128, i8),
 }
 
-#[bench]
-fn vec_append_with_decode_and_encode(b: &mut test::Bencher) {
+fn vec_append_with_decode_and_encode(b: &mut Bencher) {
 	let data = b"PCX";
 
 	b.iter(|| {
@@ -101,8 +86,7 @@ fn vec_append_with_decode_and_encode(b: &mut test::Bencher) {
 	})
 }
 
-#[bench]
-fn vec_append_with_encode_append(b: &mut test::Bencher) {
+fn vec_append_with_encode_append(b: &mut Bencher) {
 	let data = b"PCX";
 
 	b.iter(|| {
@@ -119,3 +103,45 @@ fn vec_append_with_encode_append(b: &mut test::Bencher) {
 		}
 	});
 }
+
+fn encode_decode_vec_u8(c: &mut Criterion) {
+	c.bench_function_over_inputs("vec_u8_encode - Vec<u8>", |b, &vec_size| {
+		let vec: Vec<u8> = (0..=255u8)
+			.cycle()
+			.take(vec_size)
+			.collect();
+
+		let vec = black_box(vec);
+		b.iter(|| vec.encode())
+	}, vec![1, 2, 5, 32, 1024]);
+
+	c.bench_function_over_inputs("vec_u8_decode - Vec<u8>", |b, &vec_size| {
+		let vec: Vec<u8> = (0..=255u8)
+			.cycle()
+			.take(vec_size)
+			.collect();
+
+		let vec = vec.encode();
+
+		let vec = black_box(vec);
+		b.iter(|| {
+			let _: Vec<u8> = Decode::decode(&mut &vec[..]).unwrap();
+		})
+	}, vec![1, 2, 5, 32, 1024]);
+}
+
+fn bench_fn(c: &mut Criterion) {
+	c.bench_function("vec_write_as_output", vec_write_as_output);
+	c.bench_function("vec_extend", vec_extend);
+	c.bench_function("vec_extend_from_slice", vec_extend_from_slice);
+	c.bench_function("vec_append_with_decode_and_encode", vec_append_with_decode_and_encode);
+	c.bench_function("vec_append_with_encode_append", vec_append_with_encode_append);
+	c.bench_function("array_vec_write_u128", array_vec_write_u128);
+}
+
+criterion_group!{
+	name = benches;
+	config = Criterion::default().warm_up_time(Duration::from_millis(500)).without_plots();
+	targets = encode_decode_vec_u8, bench_fn
+}
+criterion_main!(benches);
