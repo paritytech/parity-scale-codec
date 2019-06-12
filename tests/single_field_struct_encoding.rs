9a79c83e08831a78802516bdfec42f935c968e10
Andrew Jones <ascjones@gmail.com>
2019-06-12 17:07:21 +0100

    Add test for Compact<T> codec

--- tests/single_field_struct_encoding.rs
+++ tests/single_field_struct_encoding.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate parity_scale_codec_derive;
 
-use parity_scale_codec::{Encode, HasCompact, Decode};
+use parity_scale_codec::{Compact, Encode, HasCompact, Decode};
 use serde_derive::{Serialize, Deserialize};
 
 #[derive(Debug, PartialEq, Encode, Decode)]
@@ -68,6 +68,7 @@ fn test_encoding() {
 	let u = U(x);
 	let u_skip = USkip(Default::default(), x, Default::default());
 	let uc = Uc(x);
+	let ucom = Compact(u);
 	let ucas = Ucas(u);
 	let u_skip_cas = USkipcas(u_skip);
 	let s_skip_cas = SSkipcas(s_skip);
@@ -80,6 +81,7 @@ fn test_encoding() {
 	let mut u_encoded: &[u8] = &[3, 0, 0, 0];
 	let mut u_skip_encoded: &[u8] = &[3, 0, 0, 0];
 	let mut uc_encoded: &[u8] = &[12];
+	let mut ucom_encoded: &[u8] = &[12];
 	let mut ucas_encoded: &[u8] = &[12];
 	let mut u_skip_cas_encoded: &[u8] = &[12];
 	let mut s_skip_cas_encoded: &[u8] = &[12];
@@ -92,6 +94,7 @@ fn test_encoding() {
 	assert_eq!(u.encode(), u_encoded);
 	assert_eq!(u_skip.encode(), u_skip_encoded);
 	assert_eq!(uc.encode(), uc_encoded);
+	assert_eq!(ucom.encode(), ucom_encoded);
 	assert_eq!(ucas.encode(), ucas_encoded);
 	assert_eq!(u_skip_cas.encode(), u_skip_cas_encoded);
 	assert_eq!(s_skip_cas.encode(), s_skip_cas_encoded);
@@ -104,6 +107,7 @@ fn test_encoding() {
 	assert_eq!(u, U::decode(&mut u_encoded).unwrap());
 	assert_eq!(u_skip, USkip::decode(&mut u_skip_encoded).unwrap());
 	assert_eq!(uc, Uc::decode(&mut uc_encoded).unwrap());
+	assert_eq!(ucom, <Compact::<U>>::decode(&mut ucom_encoded).unwrap());
 	assert_eq!(ucas, Ucas::decode(&mut ucas_encoded).unwrap());
 	assert_eq!(u_skip_cas, USkipcas::decode(&mut u_skip_cas_encoded).unwrap());
 	assert_eq!(s_skip_cas, SSkipcas::decode(&mut s_skip_cas_encoded).unwrap());
