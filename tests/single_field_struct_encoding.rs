215cda34b75f0550248bb377c54ed0525e307c7b
Andrew Jones <ascjones@gmail.com>
2019-06-12 17:27:18 +0100

    Derive `CompactAs` for single field structs (#97)
    * Basic CompactAs derive
    
    * Support single field named structs
    
    * Extract function to wrap impl in dummy const
    
    * Support single non-skipped field
    
    * Restore original include_parity_scale_codec_crate function
    
    * Add test for Compact<T> codec

--- tests/single_field_struct_encoding.rs
+++ tests/single_field_struct_encoding.rs
@@ -1,14 +1,16 @@
 #[macro_use]
 extern crate parity_scale_codec_derive;
 
-use parity_scale_codec::{Encode, HasCompact, Decode};
+use parity_scale_codec::{Compact, Encode, HasCompact, Decode};
+use serde_derive::{Serialize, Deserialize};
 
 #[derive(Debug, PartialEq, Encode, Decode)]
 struct S {
 	x: u32,
 }
 
-#[derive(Debug, PartialEq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Encode, Decode, CompactAs)]
 struct SSkip {
 	#[codec(skip)]
 	s1: u32,
@@ -29,14 +31,29 @@ struct Sh<T: HasCompact> {
 	x: T,
 }
 
-#[derive(Debug, PartialEq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Encode, Decode, CompactAs)]
 struct U(u32);
 
-#[derive(Debug, PartialEq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Encode, Decode, CompactAs)]
+struct U2 { a: u64 }
+
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Encode, Decode, CompactAs)]
 struct USkip(#[codec(skip)] u32, u32, #[codec(skip)] u32);
 
 #[derive(Debug, PartialEq, Encode, Decode)]
 struct Uc(#[codec(compact)] u32);
+
+#[derive(Debug, PartialEq, Clone, Encode, Decode)]
+struct Ucas(#[codec(compact)] U);
+
+#[derive(Debug, PartialEq, Clone, Encode, Decode)]
+struct USkipcas(#[codec(compact)] USkip);
+
+#[derive(Debug, PartialEq, Clone, Encode, Decode)]
+struct SSkipcas(#[codec(compact)] SSkip);
 
 #[derive(Debug, PartialEq, Encode, Decode)]
 struct Uh<T: HasCompact>(#[codec(encoded_as = "<T as HasCompact>::Type")] T);
@@ -51,6 +68,10 @@ fn test_encoding() {
 	let u = U(x);
 	let u_skip = USkip(Default::default(), x, Default::default());
 	let uc = Uc(x);
+	let ucom = Compact(u);
+	let ucas = Ucas(u);
+	let u_skip_cas = USkipcas(u_skip);
+	let s_skip_cas = SSkipcas(s_skip);
 	let uh = Uh(x);
 
 	let mut s_encoded: &[u8] = &[3, 0, 0, 0];
@@ -60,6 +81,10 @@ fn test_encoding() {
 	let mut u_encoded: &[u8] = &[3, 0, 0, 0];
 	let mut u_skip_encoded: &[u8] = &[3, 0, 0, 0];
 	let mut uc_encoded: &[u8] = &[12];
+	let mut ucom_encoded: &[u8] = &[12];
+	let mut ucas_encoded: &[u8] = &[12];
+	let mut u_skip_cas_encoded: &[u8] = &[12];
+	let mut s_skip_cas_encoded: &[u8] = &[12];
 	let mut uh_encoded: &[u8] = &[12];
 
 	assert_eq!(s.encode(), s_encoded);
@@ -69,6 +94,10 @@ fn test_encoding() {
 	assert_eq!(u.encode(), u_encoded);
 	assert_eq!(u_skip.encode(), u_skip_encoded);
 	assert_eq!(uc.encode(), uc_encoded);
+	assert_eq!(ucom.encode(), ucom_encoded);
+	assert_eq!(ucas.encode(), ucas_encoded);
+	assert_eq!(u_skip_cas.encode(), u_skip_cas_encoded);
+	assert_eq!(s_skip_cas.encode(), s_skip_cas_encoded);
 	assert_eq!(uh.encode(), uh_encoded);
 
 	assert_eq!(s, S::decode(&mut s_encoded).unwrap());
@@ -78,5 +107,9 @@ fn test_encoding() {
 	assert_eq!(u, U::decode(&mut u_encoded).unwrap());
 	assert_eq!(u_skip, USkip::decode(&mut u_skip_encoded).unwrap());
 	assert_eq!(uc, Uc::decode(&mut uc_encoded).unwrap());
+	assert_eq!(ucom, <Compact::<U>>::decode(&mut ucom_encoded).unwrap());
+	assert_eq!(ucas, Ucas::decode(&mut ucas_encoded).unwrap());
+	assert_eq!(u_skip_cas, USkipcas::decode(&mut u_skip_cas_encoded).unwrap());
+	assert_eq!(s_skip_cas, SSkipcas::decode(&mut s_skip_cas_encoded).unwrap());
 	assert_eq!(uh, Uh::decode(&mut uh_encoded).unwrap());
 }
