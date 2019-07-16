5d94446976c7d2091a2378ab8c6bcbc52d81c244
thiolliere <gui.thiolliere@gmail.com>
2019-07-16 13:45:17 +0200

    add test

---
+++ tests/invalid_input_len.rs
@@ -0,0 +1,24 @@
+use parity_scale_codec::{Encode, Decode, OptionBool, Compact};
+
+// Test that not all input is decoded.
+#[test]
+fn vec_of_vec() {
+	let mut input = Compact::<u32>(16).encode();
+	input.extend(&[0; 15]);
+	let input = &mut &input[..];
+
+	let input_start_len = input.len();
+	let res = <Vec<Vec<()>>>::decode(input);
+	assert!(res.is_err());
+
+	// Decoding stopped early, before vec allocation.
+	assert_eq!(input.len(), input_start_len - 1);
+}
+
+// Test error returned.
+#[test]
+#[should_panic(expected = "Not enough data to fill buffer")]
+fn option_bool() {
+	let input: [u8; 0] = [];
+	OptionBool::decode(&mut &input[..]).unwrap();
+}
