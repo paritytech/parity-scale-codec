db362c3fe574c4ebd4dc17b5e249011f967d294e
Bastian Köcher <git@kchr.de>
2019-07-25 09:16:11 +0200

    Improve `EncodeAppend` api and clean up code

--- src/codec.rs
+++ src/codec.rs
@@ -1,4 +1,4 @@
-// Copyright 2017, 2018 Parity Technologies
+// Copyright 2017-2018 Parity Technologies
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 use crate::alloc::vec::Vec;
 use crate::alloc::boxed::Box;
 use crate::alloc::collections::{BTreeMap, BTreeSet, VecDeque, LinkedList, BinaryHeap};
-use crate::compact::{Compact, CompactLen};
+use crate::compact::Compact;
 
 #[cfg(any(feature = "std", feature = "full"))]
 use crate::alloc::{
@@ -229,16 +229,6 @@ pub trait Encode {
 	fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
 		f(&self.encode())
 	}
-}
-
-/// Trait that allows to append items to an encoded representation without
-/// decoding all previous added items.
-pub trait EncodeAppend {
-	/// The item that will be appended.
-	type Item: Encode;
-
-	/// Append `to_append` items to the given `self_encoded` representation.
-	fn append(self_encoded: Vec<u8>, to_append: &[Self::Item]) -> Result<Vec<u8>, Error>;
 }
 
 /// Trait that allows the length of a collection to be read, without having
@@ -523,6 +513,15 @@ impl Decode for String {
 	}
 }
 
+pub(crate) fn compact_encode_len_to<W: Output>(dest: &mut W, len: usize) -> Result<(), Error> {
+	if len > u32::max_value() as usize {
+		return Err("Attempted to serialize a collection with too many elements.".into());
+	}
+
+	Compact(len as u32).encode_to(dest);
+	Ok(())
+}
+
 impl<T: Encode> Encode for [T] {
 	fn size_hint(&self) -> usize {
 		if let IsU8::Yes = <T as Encode>::IS_U8 {
@@ -533,9 +532,8 @@ impl<T: Encode> Encode for [T] {
 	}
 
 	fn encode_to<W: Output>(&self, dest: &mut W) {
-		let len = self.len();
-		assert!(len <= u32::max_value() as usize, "Attempted to serialize a collection with too many elements.");
-		Compact(len as u32).encode_to(dest);
+		compact_encode_len_to(dest, self.len()).expect("Compact encodes length");
+
 		if let IsU8::Yes= <T as Encode>::IS_U8 {
 			let self_transmute = unsafe {
 				core::mem::transmute::<&[T], &[u8]>(self)
@@ -570,55 +568,6 @@ impl<T: Decode> Decode for Vec<T> {
 	}
 }
 
-impl<T: Encode + Decode> EncodeAppend for Vec<T> {
-	type Item = T;
-
-	fn append(mut self_encoded: Vec<u8>, to_append: &[Self::Item]) -> Result<Vec<u8>, Error> {
-		if self_encoded.is_empty() {
-			return Ok(to_append.encode())
-		}
-
-		let len = u32::from(Compact::<u32>::decode(&mut &self_encoded[..])?);
-		let new_len = len
-			.checked_add(to_append.len() as u32)
-			.ok_or_else(|| "New vec length greater than `u32::max_value()`.")?;
-
-		let encoded_len = Compact::<u32>::compact_len(&len);
-		let encoded_new_len = Compact::<u32>::compact_len(&new_len);
-
-		let replace_len = |dest: &mut Vec<u8>| {
-			Compact(new_len).using_encoded(|e| {
-				dest[..encoded_new_len].copy_from_slice(e);
-			})
-		};
-
-		let append_new_elems = |dest: &mut Vec<u8>| to_append.iter().for_each(|a| a.encode_to(dest));
-
-		// If old and new encoded len is equal, we don't need to copy the
-		// already encoded data.
-		if encoded_len == encoded_new_len {
-			replace_len(&mut self_encoded);
-			append_new_elems(&mut self_encoded);
-
-			Ok(self_encoded)
-		} else {
-			let prefix_size = encoded_new_len + self_encoded.len() - encoded_len;
-			let size_hint: usize = to_append.iter().map(Encode::size_hint).sum();
-
-			let mut res = Vec::with_capacity(prefix_size + size_hint);
-			unsafe { res.set_len(prefix_size); }
-
-			// Insert the new encoded len, copy the already encoded data and
-			// add the new element.
-			replace_len(&mut res);
-			res[encoded_new_len..prefix_size].copy_from_slice(&self_encoded[encoded_len..]);
-			append_new_elems(&mut res);
-
-			Ok(res)
-		}
-	}
-}
-
 macro_rules! impl_codec_through_iterator {
 	($(
 		$type:ty
@@ -627,9 +576,8 @@ macro_rules! impl_codec_through_iterator {
 	)*) => {$(
 		impl<$($encode_generics)*> Encode for $type {
 			fn encode_to<W: Output>(&self, dest: &mut W) {
-				let len = self.len();
-				assert!(len <= u32::max_value() as usize, "Attempted to serialize a collection with too many elements.");
-				Compact(len as u32).encode_to(dest);
+				compact_encode_len_to(dest, self.len()).expect("Compact encodes length");
+
 				for i in self.iter() {
 					i.encode_to(dest);
 				}
@@ -655,9 +603,7 @@ impl_codec_through_iterator! {
 
 impl<T: Encode + Ord> Encode for VecDeque<T> {
 	fn encode_to<W: Output>(&self, dest: &mut W) {
-		let len = self.len();
-		assert!(len <= u32::max_value() as usize, "Attempted to serialize a collection with too many elements.");
-		Compact(len as u32).encode_to(dest);
+		compact_encode_len_to(dest, self.len()).expect("Compact encodes length");
 
 		if let IsU8::Yes = <T as Encode>::IS_U8 {
 			let slices = self.as_slices();
@@ -704,7 +650,7 @@ macro_rules! impl_len {
 		impl<$($g),*> DecodeLength for $type<$($g),*> {
 			fn len(mut self_encoded: &[u8]) -> Result<usize, Error> {
 				usize::try_from(u32::from(Compact::<u32>::decode(&mut self_encoded)?))
-					.map_err(|_| "Failed convert decded size into usize.".into())
+					.map_err(|_| "Failed convert decoded size into usize.".into())
 			}
 		}
 	)*}
@@ -998,34 +944,6 @@ mod tests {
 		let encoded = value.encode();
 		assert_eq!(hexify(&encoded), "0c 01 02 00");
 		assert_eq!(<Vec<OptionBool>>::decode(&mut &encoded[..]).unwrap(), value);
-	}
-
-	#[test]
-	fn vec_encode_append_works() {
-		let max_value = 1_000_000;
-
-		let encoded = (0..max_value).fold(Vec::new(), |encoded, v| {
-			<Vec<u32> as EncodeAppend>::append(encoded, &[v]).unwrap()
-		});
-
-		let decoded = Vec::<u32>::decode(&mut &encoded[..]).unwrap();
-		assert_eq!(decoded, (0..max_value).collect::<Vec<_>>());
-	}
-
-	#[test]
-	fn vec_encode_append_multiple_items_works() {
-		let max_value = 1_000_000;
-
-		let encoded = (0..max_value).fold(Vec::new(), |encoded, v| {
-			<Vec<u32> as EncodeAppend>::append(encoded, &[v, v, v, v]).unwrap()
-		});
-
-		let decoded = Vec::<u32>::decode(&mut &encoded[..]).unwrap();
-		let expected = (0..max_value).fold(Vec::new(), |mut vec, i| {
-			vec.append(&mut vec![i, i, i, i]);
-			vec
-		});
-		assert_eq!(decoded, expected);
 	}
 
 	fn test_encode_length<T: Encode + Decode + DecodeLength>(thing: &T, len: usize) {
