cb4c8add34b4cd3456cd1406511187116417f43c
Wei Tang <hi@that.world>
2019-07-25 21:10:08 +0200

    Add tests for generic-array

--- src/generic_array.rs
+++ src/generic_array.rs
@@ -37,3 +37,28 @@ impl<T: Decode, L: generic_array::ArrayLength<T>> Decode for generic_array::Gene
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use generic_array::{GenericArray, arr, arr_impl};
+
+	#[test]
+	fn generic_array() {
+		let test = arr![u8; 3, 4, 5];
+		let encoded = test.encode();
+		assert_eq!(test, GenericArray::<u8, typenum::U3>::decode(&mut &encoded[..]).unwrap());
+
+		let test = arr![u16; 3, 4, 5, 6, 7, 8, 0];
+		let encoded = test.encode();
+		assert_eq!(test, GenericArray::<u16, typenum::U7>::decode(&mut &encoded[..]).unwrap());
+
+		let test = arr![u32; 3, 4, 5, 0, 1];
+		let encoded = test.encode();
+		assert_eq!(test, GenericArray::<u32, typenum::U5>::decode(&mut &encoded[..]).unwrap());
+
+		let test = arr![u64; 3];
+		let encoded = test.encode();
+		assert_eq!(test, GenericArray::<u64, typenum::U1>::decode(&mut &encoded[..]).unwrap());
+	}
+}
