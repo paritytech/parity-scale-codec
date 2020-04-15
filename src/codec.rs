ea8a3dd2d7014c21278544ba2a07376edfaf16b7
thiolliere <gui.thiolliere@gmail.com>
2020-04-15 18:18:36 +0200

    improve message

--- src/codec.rs
+++ src/codec.rs
@@ -340,7 +340,8 @@ pub trait Decode: Sized {
 	/// Attempt to skip the value from input.
 	///
 	/// WARNING: This doesn't check that the value is valid. If `(A, B)` is encoded, skipping A and
-	/// decoding B can succeed while decoding A would fail.
+	/// decoding B can succeed while decoding A would fail. And decoding B can fail because A is
+	/// incorrect but successfully skipped.
 	fn skip<I: Input>(value: &mut I) -> Result<(), Error>;
 }
 
