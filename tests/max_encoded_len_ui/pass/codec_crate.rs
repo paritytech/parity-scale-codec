e042072015dae4763e0f9f4562f7e05e3abf79e8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-06-24 13:15:30 +0200

    actually test the pass test

--- tests/max_encoded_len_ui/pass/codec_crate.rs
+++ tests/max_encoded_len_ui/pass/codec_crate.rs
@@ -1,6 +1,6 @@
-use parity_scale_codec::{self as codec, Encode, MaxEncodedLen};
-
 //! This test case demonstrates correct use of the `#[codec(crate = path)]` attribute.
+
+use parity_scale_codec::{self as codec, Encode, MaxEncodedLen};
 
 #[derive(Encode, MaxEncodedLen)]
 #[codec(crate = codec)]
