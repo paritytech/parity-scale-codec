799340b55c3e7adb04e21795ccd21b25810fb33f
Bastian Köcher <info@kchr.de>
2022-01-28 21:05:10 +0100

    Upgrade to BitVec 1.0

--- src/bit_vec.rs
+++ src/bit_vec.rs
@@ -15,52 +15,58 @@
 //! `BitVec` specific serialization.
 
 use bitvec::{
-	vec::BitVec, store::BitStore, order::BitOrder, slice::BitSlice, boxed::BitBox, mem::BitMemory
+	vec::BitVec, store::BitStore, order::BitOrder, slice::BitSlice, boxed::BitBox,
 };
 use crate::{
 	EncodeLike, Encode, Decode, Input, Output, Error, Compact,
 	codec::{decode_vec_with_len, encode_slice_no_len},
 };
 
-impl<O: BitOrder, T: BitStore + Encode> Encode for BitSlice<O, T> {
+impl<O: BitOrder, T: BitStore> Encode for BitSlice<T, O>
+	where
+		T::Mem: Encode
+{
 	fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
-		let len = self.len();
+		let bits = self.len();
 		assert!(
-			len <= u32::max_value() as usize,
-			"Attempted to serialize a collection with too many elements.",
+			bits <= ARCH32BIT_BITSLICE_MAX_BITS,
+			"Attempted to encode a BitSlice with too many bits.",
 		);
-		Compact(len as u32).encode_to(dest);
+		Compact(bits as u32).encode_to(dest);
 
-		// NOTE: doc of `BitSlice::as_slice`:
-		// > The returned slice handle views all elements touched by self
-		//
-		// Thus we are sure the slice doesn't contain unused elements at the end.
-		let slice = self.as_slice();
+		for element in self.domain() {
+			element.encode_to(dest);
+		}
+	}
+}
 
+impl<O: BitOrder, T: BitStore + Encode> Encode for BitVec<T, O> {
+	fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
+		let bits = self.len();
+		assert!(
+			bits <= ARCH32BIT_BITSLICE_MAX_BITS,
+			"Attempted to encode a BitVec with too many bits.",
+		);
+		Compact(bits as u32).encode_to(dest);
+
+		let slice = self.as_raw_slice();
 		encode_slice_no_len(slice, dest)
 	}
 }
 
-impl<O: BitOrder, T: BitStore + Encode> Encode for BitVec<O, T> {
-	fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
-		self.as_bitslice().encode_to(dest)
-	}
-}
-
-impl<O: BitOrder, T: BitStore + Encode> EncodeLike for BitVec<O, T> {}
+impl<O: BitOrder, T: BitStore + Encode> EncodeLike for BitVec<T, O> {}
 
 /// Equivalent of `BitStore::MAX_BITS` on 32bit machine.
 const ARCH32BIT_BITSLICE_MAX_BITS: usize = 0x1fff_ffff;
 
-impl<O: BitOrder, T: BitStore + Decode> Decode for BitVec<O, T> {
+impl<O: BitOrder, T: BitStore + Decode> Decode for BitVec<T, O> {
 	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
 		<Compact<u32>>::decode(input).and_then(move |Compact(bits)| {
 			// Otherwise it is impossible to store it on 32bit machine.
 			if bits as usize > ARCH32BIT_BITSLICE_MAX_BITS {
-				return Err("Attempt to decode a bitvec with too many bits".into());
+				return Err("Attempt to decode a BitVec with too many bits".into());
 			}
-			let required_elements = required_elements::<T>(bits)? as usize;
-			let vec = decode_vec_with_len(input, required_elements)?;
+			let vec = decode_vec_with_len(input, bitvec::mem::elts::<T>(bits as usize))?;
 
 			let mut result = Self::try_from_vec(vec)
 				.map_err(|_| {
@@ -76,30 +82,26 @@ impl<O: BitOrder, T: BitStore + Decode> Decode for BitVec<O, T> {
 	}
 }
 
-impl<O: BitOrder, T: BitStore + Encode> Encode for BitBox<O, T> {
+impl<O: BitOrder, T: BitStore + Encode> Encode for BitBox<T, O> {
 	fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
-		self.as_bitslice().encode_to(dest)
+		let bits = self.len();
+		assert!(
+			bits <= ARCH32BIT_BITSLICE_MAX_BITS,
+			"Attempted to encode a BitBox with too many bits.",
+		);
+		Compact(bits as u32).encode_to(dest);
+
+		let slice = self.as_raw_slice();
+		encode_slice_no_len(slice, dest)
 	}
 }
 
-impl<O: BitOrder, T: BitStore + Encode> EncodeLike for BitBox<O, T> {}
+impl<O: BitOrder, T: BitStore + Encode> EncodeLike for BitBox<T, O> {}
 
-impl<O: BitOrder, T: BitStore + Decode> Decode for BitBox<O, T> {
+impl<O: BitOrder, T: BitStore + Decode> Decode for BitBox<T, O> {
 	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
-		Ok(Self::from_bitslice(BitVec::<O, T>::decode(input)?.as_bitslice()))
+		Ok(BitVec::<T, O>::decode(input)?.into())
 	}
-}
-
-/// Calculates the number of element `T` required to store given amount of `bits` as if they were
-/// stored in `BitVec<_, T>`
-///
-/// Returns an error if the number of bits + number of bits in element overflow u32 capacity.
-/// NOTE: this should never happen if `bits` is already checked to be less than
-/// `BitStore::MAX_BITS`.
-fn required_elements<T: BitStore>(bits: u32) -> Result<u32, Error> {
-	let element_bits = T::Mem::BITS as u32;
-	let error = Error::from("Attempt to decode bitvec with too many bits");
-	Ok((bits.checked_add(element_bits).ok_or_else(|| error)?  - 1) / element_bits)
 }
 
 #[cfg(test)]
@@ -111,130 +113,94 @@ mod tests {
 	macro_rules! test_data {
 		($inner_type:ident) => (
 			[
-				BitVec::<Msb0, $inner_type>::new(),
-				bitvec![Msb0, $inner_type; 0],
-				bitvec![Msb0, $inner_type; 1],
-				bitvec![Msb0, $inner_type; 0, 0],
-				bitvec![Msb0, $inner_type; 1, 0],
-				bitvec![Msb0, $inner_type; 0, 1],
-				bitvec![Msb0, $inner_type; 1, 1],
-				bitvec![Msb0, $inner_type; 1, 0, 1],
-				bitvec![Msb0, $inner_type; 0, 1, 0, 1, 0, 1, 1],
-				bitvec![Msb0, $inner_type; 0, 1, 0, 1, 0, 1, 1, 0],
-				bitvec![Msb0, $inner_type; 1, 1, 0, 1, 0, 1, 1, 0, 1],
-				bitvec![Msb0, $inner_type; 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0],
-				bitvec![Msb0, $inner_type; 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0],
-				bitvec![Msb0, $inner_type; 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0],
-				bitvec![Msb0, $inner_type; 0; 15],
-				bitvec![Msb0, $inner_type; 1; 16],
-				bitvec![Msb0, $inner_type; 0; 17],
-				bitvec![Msb0, $inner_type; 1; 31],
-				bitvec![Msb0, $inner_type; 0; 32],
-				bitvec![Msb0, $inner_type; 1; 33],
-				bitvec![Msb0, $inner_type; 0; 63],
-				bitvec![Msb0, $inner_type; 1; 64],
-				bitvec![Msb0, $inner_type; 0; 65],
-				bitvec![Msb0, $inner_type; 1; MAX_PREALLOCATION * 8 + 1],
-				bitvec![Msb0, $inner_type; 0; MAX_PREALLOCATION * 9],
-				bitvec![Msb0, $inner_type; 1; MAX_PREALLOCATION * 32 + 1],
-				bitvec![Msb0, $inner_type; 0; MAX_PREALLOCATION * 33],
+				BitVec::<$inner_type, Msb0>::new(),
+				bitvec![$inner_type, Msb0; 0],
+				bitvec![$inner_type, Msb0; 1],
+				bitvec![$inner_type, Msb0; 0, 0],
+				bitvec![$inner_type, Msb0; 1, 0],
+				bitvec![$inner_type, Msb0; 0, 1],
+				bitvec![$inner_type, Msb0; 1, 1],
+				bitvec![$inner_type, Msb0; 1, 0, 1],
+				bitvec![$inner_type, Msb0; 0, 1, 0, 1, 0, 1, 1],
+				bitvec![$inner_type, Msb0; 0, 1, 0, 1, 0, 1, 1, 0],
+				bitvec![$inner_type, Msb0; 1, 1, 0, 1, 0, 1, 1, 0, 1],
+				bitvec![$inner_type, Msb0; 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0],
+				bitvec![$inner_type, Msb0; 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0],
+				bitvec![$inner_type, Msb0; 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0],
+				bitvec![$inner_type, Msb0; 0; 15],
+				bitvec![$inner_type, Msb0; 1; 16],
+				bitvec![$inner_type, Msb0; 0; 17],
+				bitvec![$inner_type, Msb0; 1; 31],
+				bitvec![$inner_type, Msb0; 0; 32],
+				bitvec![$inner_type, Msb0; 1; 33],
+				bitvec![$inner_type, Msb0; 0; 63],
+				bitvec![$inner_type, Msb0; 1; 64],
+				bitvec![$inner_type, Msb0; 0; 65],
+				bitvec![$inner_type, Msb0; 1; MAX_PREALLOCATION * 8 + 1],
+				bitvec![$inner_type, Msb0; 0; MAX_PREALLOCATION * 9],
+				bitvec![$inner_type, Msb0; 1; MAX_PREALLOCATION * 32 + 1],
+				bitvec![$inner_type, Msb0; 0; MAX_PREALLOCATION * 33],
 			]
 		)
-	}
-
-	#[test]
-	fn required_items_test() {
-		assert_eq!(Ok(0), required_elements::<u8>(0));
-		assert_eq!(Ok(1), required_elements::<u8>(1));
-		assert_eq!(Ok(1), required_elements::<u8>(7));
-		assert_eq!(Ok(1), required_elements::<u8>(8));
-		assert_eq!(Ok(2), required_elements::<u8>(9));
-
-		assert_eq!(Ok(0), required_elements::<u16>(0));
-		assert_eq!(Ok(1), required_elements::<u16>(1));
-		assert_eq!(Ok(1), required_elements::<u16>(15));
-		assert_eq!(Ok(1), required_elements::<u16>(16));
-		assert_eq!(Ok(2), required_elements::<u16>(17));
-
-		assert_eq!(Ok(0), required_elements::<u32>(0));
-		assert_eq!(Ok(1), required_elements::<u32>(1));
-		assert_eq!(Ok(1), required_elements::<u32>(31));
-		assert_eq!(Ok(1), required_elements::<u32>(32));
-		assert_eq!(Ok(2), required_elements::<u32>(33));
-
-		assert_eq!(Ok(0), required_elements::<u64>(0));
-		assert_eq!(Ok(1), required_elements::<u64>(1));
-		assert_eq!(Ok(1), required_elements::<u64>(63));
-		assert_eq!(Ok(1), required_elements::<u64>(64));
-		assert_eq!(Ok(2), required_elements::<u64>(65));
 	}
 
 	#[test]
 	fn bitvec_u8() {
 		for v in &test_data!(u8) {
 			let encoded = v.encode();
-			assert_eq!(*v, BitVec::<Msb0, u8>::decode(&mut &encoded[..]).unwrap());
+			assert_eq!(*v, BitVec::<u8, Msb0>::decode(&mut &encoded[..]).unwrap());
+
+			let encoded = v.as_bitslice().encode();
+			assert_eq!(*v, BitVec::<u8, Msb0>::decode(&mut &encoded[..]).unwrap());
 		}
 	}
 
 	#[test]
-	// Flaky test due to:
-	// * https://github.com/bitvecto-rs/bitvec/issues/135
-	// * https://github.com/rust-lang/miri/issues/1866
-	#[cfg(not(miri))]
-
 	fn bitvec_u16() {
 		for v in &test_data!(u16) {
 			let encoded = v.encode();
-			assert_eq!(*v, BitVec::<Msb0, u16>::decode(&mut &encoded[..]).unwrap());
+			assert_eq!(*v, BitVec::<u16, Msb0>::decode(&mut &encoded[..]).unwrap());
+
+			let encoded = v.as_bitslice().encode();
+			assert_eq!(*v, BitVec::<u16, Msb0>::decode(&mut &encoded[..]).unwrap());
 		}
 	}
 
 	#[test]
-	// Flaky test due to:
-	// * https://github.com/bitvecto-rs/bitvec/issues/135
-	// * https://github.com/rust-lang/miri/issues/1866
-	#[cfg(not(miri))]
 	fn bitvec_u32() {
 		for v in &test_data!(u32) {
 			let encoded = v.encode();
-			assert_eq!(*v, BitVec::<Msb0, u32>::decode(&mut &encoded[..]).unwrap());
+			assert_eq!(*v, BitVec::<u32, Msb0>::decode(&mut &encoded[..]).unwrap());
+
+			let encoded = v.as_bitslice().encode();
+			assert_eq!(*v, BitVec::<u32, Msb0>::decode(&mut &encoded[..]).unwrap());
 		}
 	}
 
 	#[test]
-	// Flaky test due to:
-	// * https://github.com/bitvecto-rs/bitvec/issues/135
-	// * https://github.com/rust-lang/miri/issues/1866
-	#[cfg(not(miri))]
-
 	fn bitvec_u64() {
 		for v in &test_data!(u64) {
 			let encoded = v.encode();
-			assert_eq!(*v, BitVec::<Msb0, u64>::decode(&mut &encoded[..]).unwrap());
+			assert_eq!(*v, BitVec::<u64, Msb0>::decode(&mut &encoded[..]).unwrap());
 		}
 	}
 
 	#[test]
 	fn bitslice() {
 		let data: &[u8] = &[0x69];
-		let slice = BitSlice::<Msb0, u8>::from_slice(data).unwrap();
+		let slice = BitSlice::<u8, Msb0>::from_slice(data);
 		let encoded = slice.encode();
-		let decoded = BitVec::<Msb0, u8>::decode(&mut &encoded[..]).unwrap();
+		let decoded = BitVec::<u8, Msb0>::decode(&mut &encoded[..]).unwrap();
 		assert_eq!(slice, decoded.as_bitslice());
 	}
 
 	#[test]
-	// Flaky test due to:
-	// * https://github.com/bitvecto-rs/bitvec/issues/135
-	// * https://github.com/rust-lang/miri/issues/1866
-	#[cfg(not(miri))]
 	fn bitbox() {
 		let data: &[u8] = &[5, 10];
-		let slice = BitSlice::<Msb0, u8>::from_slice(data).unwrap();
-		let bb = BitBox::<Msb0, u8>::from_bitslice(slice);
+		let slice = BitSlice::<u8, Msb0>::from_slice(data);
+		let bb = BitBox::<u8, Msb0>::from_bitslice(slice);
 		let encoded = bb.encode();
-		let decoded = BitBox::<Msb0, u8>::decode(&mut &encoded[..]).unwrap();
+		let decoded = BitBox::<u8, Msb0>::decode(&mut &encoded[..]).unwrap();
 		assert_eq!(bb, decoded);
 	}
 }
