0e8165d0e41144c10239921041269962144158b7
Keith Yeung <kungfukeith11@gmail.com>
2022-06-02 16:13:48 +0100

    Update derive/src/max_encoded_len.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- derive/src/max_encoded_len.rs
+++ derive/src/max_encoded_len.rs
@@ -42,7 +42,7 @@ pub fn derive_max_encoded_len(input: proc_macro::TokenStream) -> proc_macro::Tok
 		custom_mel_trait_bound(&input.attrs),
 		parse_quote!(#crate_path::MaxEncodedLen),
 		None,
-		true,
+		utils::has_dumb_trait_bound(&input.attrs),
 		&crate_path
 	) {
 		return e.to_compile_error().into()
