30933075c9b1cedc546d62b9a8a7d7fee5dd2f65
Eran Rundstein <eran@rundste.in>
2023-08-17 12:12:15 -0700

    Apply suggestions from code review
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- src/compact.rs
+++ src/compact.rs
@@ -192,12 +192,14 @@ impl<T> core::fmt::Debug for Compact<T> where T: core::fmt::Debug {
 	}
 }
 
+#[cfg(feature = "serde")]
 impl<T> serde::Serialize for Compact<T> where T: serde::Serialize {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
 		T::serialize(&self.0, serializer)
 	}
 }
 
+#[cfg(feature = "serde")]
 impl<'de, T> serde::Deserialize<'de> for Compact<T> where T: serde::Deserialize<'de> {
 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
 		T::deserialize(deserializer).map(Compact)
