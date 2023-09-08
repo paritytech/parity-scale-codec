1e3f80c1e047ec67f79f12e6a8067692edb14e9d
PG Herveou <pgherveou@parity.io>
2023-09-08 11:02:20 +0200

    Update MaxEncodedLen derive macro (#512)
    Fix the generated code for MaxEncoded derive trait
    
    ---------
    
    Co-authored-by: Bastian Köcher <info@kchr.de>

--- src/max_encoded_len.rs
+++ src/max_encoded_len.rs
@@ -46,9 +46,8 @@ macro_rules! impl_primitives {
 	};
 }
 
-impl_primitives!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, bool);
-
 impl_primitives!(
+	u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, bool,
 	NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU128, NonZeroI8, NonZeroI16, NonZeroI32,
 	NonZeroI64, NonZeroI128
 );
