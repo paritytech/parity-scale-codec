f6429a95e1b4f53c57bc70b31af428608ba3fd4f
David Emett <david.emett@parity.io>
2025-07-29 15:59:44 +0100

    UI test: MaxEncodedLen derive with NotMel field

---
+++ tests/max_encoded_len_ui/unsupported_field.rs
@@ -0,0 +1,12 @@
+use parity_scale_codec::{Encode, MaxEncodedLen};
+
+#[derive(Encode)]
+struct NotMel;
+
+#[derive(Encode, MaxEncodedLen)]
+struct UnsupportedField {
+	mel: u32,
+	not_mel: NotMel,
+}
+
+fn main() {}
